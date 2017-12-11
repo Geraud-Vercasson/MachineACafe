5dde4cf1ee3f65e7389ac91881ec5b8d87bbf9e4
Géraud Vercasson <geraud.vercasson@Digital-Grenoble.local>
2017-12-11 12:08:07 +0100

    v1

---
+++ script.js
@@ -0,0 +1,36 @@
+/*Déclaration des variables*/
+
+let compteur = 0;
+let nbSucres = 1;
+
+/*Déclaration des fonctions */
+
+function resetDrink(){
+
+    $('#monnayeur').html("Test");
+    
+}
+
+function selectDrink(doSelect, drink){
+
+}
+
+function addSugar(){
+
+}
+
+function removeSugar(){
+
+}
+
+function addCoin(coin){
+
+}
+
+function resetCoin(){
+
+}
+
+$(document).ready(function(){
+    $('btnResetCoin').click(resetDrink());
+});
